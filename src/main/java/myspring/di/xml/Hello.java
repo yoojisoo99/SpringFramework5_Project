80421c9918c9d7e681d3cce3ca2aad167953af7e
yoojisoo99 <yoojisoo99@naver.com>
2026-03-04 16:58:07 +0900

    first commit

---
+++ src/main/java/myspring/di/xml/Hello.java
@@ -0,0 +1,42 @@
+package myspring.di.xml;
+
+import java.util.List;
+
+public class Hello {
+	String name;
+	Printer printer;
+	List<String> names;
+
+	public Hello() {
+	}
+
+	public Hello(String name, Printer printer) {
+		this.name = name;
+		this.printer = printer;
+	}
+
+	public List<String> getNames() {
+		return this.names;
+	}
+
+	public void setNames(List<String> list) {
+		this.names = list;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrinter(Printer printer) {
+		this.printer = printer;
+	}
+
+	public String sayHello() {
+		return "Hello " + name;
+	}
+
+	public void print() {
+		this.printer.print(sayHello());
+	}
+
+}
